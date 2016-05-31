ffa0ceb89113e1d15a28a9ebb8b797003d472120
Pavel Dubov <dubov94@gmail.com>
2016-05-31 03:28:57 +0300

    Fix hq and bitrate chemistry

--- audio_bitrate.js
+++ audio_bitrate.js
@@ -24,23 +24,25 @@ addFeature(new function() {
                     }`
             } else {
                 css.innerHTML =
-                    `.vk_extensions_duration {
-                        display: block !important;
-                    }
-                    .vk_extensions_duration { 
-                        margin-top: -4px;
-                    }
-                    .audio_row_current.inlined .vk_extensions_duration {
-                        margin-top: -7px;
+                    `.vk_extensions_audio .audio_duration {
+                        display: block;
+                        margin-top: -8px;
                     }
 
-                    .vk_extensions_bitrate { 
-                        font-size: 0.7em; 
-                        line-height: calc(1em + 1px); 
+                    .vk_extensions_audio .audio_duration + div { 
+                        font-size: 0.8em;  
                         text-align: right; 
                     }
-                    .audio_row:hover .vk_extensions_bitrate {
-                        display: none;
+
+                    .vk_extensions_audio:not(.inlined) .audio_hq_label + div {
+                        position: relative;
+                        top: 2px;
+                    }
+                    .vk_extensions_audio .audio_hq_label + div {
+                        display: inline-block !important;
+                    }
+                    .vk_extensions_audio:hover .audio_hq_label + div {
+                        display: none !important;
                     }`
             }
             document.head.appendChild(css)
@@ -94,19 +96,27 @@ addFeature(new function() {
             window.addEventListener('message', function(event) {
                 if(event.data.type === 'vkExtensionsAudioBitrate') {
                     let audio = event.data.audio
-                    let audioBlock = document.querySelector('#audio_' + audio.fullId)
-                    let durationWrapper = audioBlock.querySelector('.audio_duration_wrap')
-
                     let xhr = new XMLHttpRequest()
                     xhr.open('HEAD', audio.url, true)
                     xhr.onreadystatechange = function() {
                         if(this.readyState == this.DONE && this.status == 200) {
-                            let bitrate = Math.round(Number(this.getResponseHeader('Content-Length')) * 8 / audio.duration / 1000)
-                            let bitrateBlock = document.createElement('div')
-                            bitrateBlock.classList.add('vk_extensions_bitrate')
-                            bitrateBlock.innerText = bitrate
-                            durationWrapper.children[1].classList.add('vk_extensions_duration')
-                            durationWrapper.appendChild(bitrateBlock)
+                            let audioBlocks = document.querySelectorAll('#audio_' + audio.fullId)
+                            for(let i = 0; i < audioBlocks.length; ++i) {
+                                if(audioBlocks[i].classList.contains('vk_extensions_audio_processed')) {
+                                    audioBlocks[i].classList.add('vk_extensions_audio')
+                                    let durationBlock = audioBlocks[i].querySelector('.audio_duration')
+                                    if(durationBlock.parentNode.classList.length != 0) {
+                                        let bitrate = Math.round(Number(this.getResponseHeader('Content-Length')) * 8 / audio.duration / 1000)
+                                        let wrapper = document.createElement('div')
+                                        let bitrateBlock = document.createElement('div')
+                                        durationBlock.parentNode.insertBefore(wrapper, durationBlock)
+                                        durationBlock.remove()
+                                        wrapper.appendChild(durationBlock)
+                                        bitrateBlock.innerHTML = bitrate
+                                        wrapper.appendChild(bitrateBlock)
+                                    }
+                                }
+                            }
                         }
                     }
                     xhr.send()
@@ -114,25 +124,29 @@ addFeature(new function() {
             })
 
             contentChanged = function() {
-                let blocksNotProcessed = document.querySelectorAll('.audio_row:not(.vk_extensions_audio)')
+                let blocksNotProcessed = document.querySelectorAll('.audio_row:not(.vk_extensions_audio_processed)')
                 if(blocksNotProcessed.length > 0) {
                     let idsNotProcessed = []
                     for(let i = 0; i < blocksNotProcessed.length; ++i) {
                         idsNotProcessed.push(blocksNotProcessed[i].getAttribute('data-full-id'))
-                        blocksNotProcessed[i].classList.add('vk_extensions_audio')
+                        blocksNotProcessed[i].classList.add('vk_extensions_audio_processed')
                     }
                     getAudioData(idsNotProcessed, 'vkExtensionsAudioBitrate')
                 }
             }
 
             undo = function() {
-                let blocksProcessed = document.querySelectorAll('.vk_extensions_audio')
+                let blocksProcessed = document.querySelectorAll('.vk_extensions_audio_processed')
                 for(let i = 0; i < blocksProcessed.length; ++i) {
-                    let audioBlock = blocksProcessed[i]
-                    let durationWrapper = audioBlock.querySelector('.audio_duration_wrap')
-                    durationWrapper.removeChild(durationWrapper.lastElementChild)
-                    durationWrapper.firstElementChild.classList.remove('vk_extensions_duration')
-                    audioBlock.classList.remove('vk_extensions_audio')
+                    blocksProcessed[i].classList.remove('vk_extensions_audio_processed')
+                    blocksProcessed[i].classList.remove('vk_extensions_audio')
+                    let durationBlock = blocksProcessed[i].querySelector('.audio_duration')
+                    let wrapper = durationBlock.parentNode
+                    if(wrapper.classList.length == 0) {
+                        durationBlock.remove()
+                        wrapper.parentNode.insertBefore(durationBlock, wrapper)
+                        wrapper.remove()
+                    }
                 }
             }
         }
